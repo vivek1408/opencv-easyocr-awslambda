349dadc872eccdc775dd4e77bdaf579f4825695b
vivek1408 <47197744+vivek1408@users.noreply.github.com>
2021-10-18 12:45:57 +0530

    first commit

---
+++ easy_ocr.py
@@ -0,0 +1,209 @@
+# USAGE
+# python easy_ocr.py --image images/arabic_sign.jpg --langs en,ar
+
+# import the necessary packages
+from easyocr import Reader
+import cv2
+import time
+import pandas as pd
+
+def cleanup_text(text):
+    # strip out non-ASCII text so we can draw the text on the image
+    # using OpenCV
+    return "".join([c if ord(c) < 128 else "" for c in text]).strip()
+
+
+def ocr(img, c1, c2, c3, c4, c5, c6, c7, c8):
+
+    print("[INFO] OCR'ing input image...")
+    # break the input languages into a comma separated list
+    # 今回は日本語がないので言語リストは英語だけに設定しておいてんも構わないです。
+    langs = ["en"]
+    print("[INFO] OCR'ing with the following languages: {}".format(langs))
+    # Divide_form_cols関数で見つけた座標データによって列に分ける
+    col1 = img[:,c1:c2]  # 検査項目の列
+    col2 = img[:,c3:c4]  # 結果の列
+    col3 = img[:,c5:c6]  # 単位の列
+    col4 = img[:,c7:c8]  # 結果のL/Hの列
+    x, y = col2.shape[:2]
+    reader = Reader(langs, False)
+    t1 = time.time()
+    results1 = reader.readtext(col1, paragraph=False, detail=1)
+    # Min_sizeは2ピクセル以下の幅の文字を無視するようなパラメータです。
+    # width_thsは並んでいる文字を別々に認識するためのパラメータです。0.1に設定したら少し空いていても別々で読み込むが、0.8でしたら結構スペースが入っていても一つの言葉として読み取る。
+    #Allow_Listは指定した文字の中からOCRする。
+    results2 = reader.readtext(col2, paragraph=False, detail=1, min_size= 2,  width_ths= 0.8, allowlist= "1234567890.")
+    results3 = reader.readtext(col3, paragraph=False, detail=1, width_ths= 0.8, allowlist= "1234567890^-/uL")
+    results4 = reader.readtext(col4, paragraph=False, detail=1, width_ths= 0.8, allowlist= "HL")
+
+    t2 = time.time()
+    print("\n\n time taken = %5.3f" % (t2 - t1))
+    # 下記のコードはBoundingBoxが書いたイメージを出力するためです。
+    # for (bbox, text, prob) in results:
+    #     # display the OCR'd text and associated probability
+    #     print("[INFO]  {}".format(text))
+    #     # unpack the bounding box
+    #     (tl, tr, br, bl) = bbox
+    #     tl = (int(tl[0]), int(tl[1]))
+    #     tr = (int(tr[0]), int(tr[1]))
+    #     br = (int(br[0]), int(br[1]))
+    #     bl = (int(bl[0]), int(bl[1]))
+    #
+    #     # cleanup the text and draw the box surrounding the text along
+    #     # with the OCR'd text itself
+    #     text = cleanup_text(text)
+    #     cv2.rectangle(col3, tl, br, (0, 255, 0), 1)
+    #     cv2.putText(col3, text, (tl[0] - 10, tl[1]),
+    #                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+
+    # Easyocrの結果リストのタイプからPandasデータフレームに変更する。データの簡単操作のためです。
+
+    df1 = pd.DataFrame(results1, columns=["BBox", "Text", "Prob"])
+    df2 = pd.DataFrame(results2, columns=["BBox", "Text", "Prob"])
+    df3 = pd.DataFrame(results3, columns=["BBox", "Text", "Prob"])
+    df4 = pd.DataFrame(results4, columns=["BBox", "Text", "Prob"])
+    # df3["TLx"] = df3["BBox"].apply(lambda x: x[0][0])
+    # BRyはBounding boxの右下のY座標です。このY座標によって読み取りたい分だけを切り取る。
+    df1["BRy"] = df1["BBox"].apply(lambda x :x[2][1])
+    df2["BRy"] = df2["BBox"].apply(lambda x: x[2][1])
+    df3["BRy"] = df3["BBox"].apply(lambda x: x[2][1])
+    df4["BRy"] = df4["BBox"].apply(lambda x: x[2][1])
+    # OCRの結果をdiskに保存する。
+    df1[["Text", "BRy"]].to_csv("C:\\Users\\shimadzu\\Documents\\t-icu\\col1.csv",encoding='utf-8-sig')
+    df2[["Text", "BRy"]].to_csv("C:\\Users\\shimadzu\\Documents\\t-icu\\col2.csv",encoding='utf-8-sig')
+    df3[["Text", "BRy"]].to_csv("C:\\Users\\shimadzu\\Documents\\t-icu\\col3.csv",encoding='utf-8-sig')
+    df4[["Text", "BRy"]].to_csv("C:\\Users\\shimadzu\\Documents\\t-icu\\col4.csv",encoding='utf-8-sig')
+
+    #cv2.imshow("Image", col2)
+    #cv2.waitKey(0)
+
+
+def divide_form_cols(im):
+    # Column毎に分けるために最小のRowだけをよみとっても結構ですのでイメージをCropしてOCRに進む。
+    # これはOCRの時間を削減するためです。
+    crop = im[:200, :]
+    # n言語リストを設定するとき、日本語と英語に設定する。
+    langs = ["ja", "en"]
+    print("Performing initial OCR to figure out rows & Columns")
+    reader = Reader(langs, False)
+    # OCR時間を測定するため。
+    t1 = time.time()
+    # Paragraph = False の設定は読み取った文字を文章にしないようにするパラメータです。
+    # Detail =Trueは読み取った文字だけでなく文字のBoundingBoxと結果の信頼性を返すようなパラメータです。
+    results = reader.readtext(crop, paragraph=False, detail=1)
+    t2 = time.time()
+    print("\n\n time taken = %5.3f" % (t2 - t1))
+    # Easyocrの結果リストのタイプからPandasデータフレームに変更する。データの簡単操作のためです。
+    df = pd.DataFrame(results, columns=["BBox", "Text", "Prob"])
+    # 文字のBounding boxから左上のX座標と右下のX座標を切り取って新たなColumnで保存する。
+    df["TLx"] = df["BBox"].apply(lambda x :x[0][0])
+    df["BRx"] = df["BBox"].apply(lambda x :x[2][0])
+    # ”No”のColumnを探してそれの右下のX座標を取りプラス30ピクセルを一つ目のColumn1の最初値という設定をする。
+    # （30ピクセルという値は手動でPaintで画像を開いて分析して分かったものです。）
+    No_xcoord_BR = df.loc[df["Text"] == "No", "BRx"]
+    col1_start = No_xcoord_BR + 15*2
+    item_xcoord_BR = df.loc[df["Text"] == "検査項目", "BRx"]
+    # 同じ様にColum1のエンドは検査項目の右下のX座標＋200ピクセルという定義をする。
+    col1_end = item_xcoord_BR + 100*2
+    result_xcoord_TL = df.loc[df["Text"] == "結果", "TLx"]
+    col2_start = result_xcoord_TL
+    result_xcoord_BR = df.loc[df["Text"] == "結果", "BRx"]
+    col2_end = result_xcoord_BR + 100
+    units_xcoord_TL = df.loc[df["Text"] == "単位名称", "TLx"]
+    col3_start = units_xcoord_TL - 110*2
+    units_xcoord_BR = df.loc[df["Text"] == "単位名称", "BRx"]
+    col3_end = units_xcoord_BR + 15*2
+    col4_start = result_xcoord_BR+60*2
+    col4_end = result_xcoord_BR + 100*2
+    print (col1_start, col1_end, col2_start, col2_end, col3_start, col3_end, col4_start, col4_end)
+
+    return int(col1_start), int(col1_end), int(col2_start), int(col2_end), int(col3_start), int(col3_end), int(col4_start), int(col4_end)
+
+
+def find_reqd_text():
+
+    df1 = pd.read_csv("C:\\Users\\shimadzu\\Documents\\t-icu\\col1.csv")
+    df2 = pd.read_csv("C:\\Users\\shimadzu\\Documents\\t-icu\\col2.csv")
+    df3 = pd.read_csv("C:\\Users\\shimadzu\\Documents\\t-icu\\col3.csv")
+    df4 = pd.read_csv("C:\\Users\\shimadzu\\Documents\\t-icu\\col4.csv")
+    # OCRした資料から必要な列だけを探して読み取る。
+    # 必要な列は項目がPLTやT-Bilになっているところだけですので、それを指定します。
+    col1_reqd = df1.loc[(df1["Text"] == "T-Bi l")| (df1["Text"] == "T-Bil")|(df1["Text"] == "PLT")|(df1["Text"] == "CRE"), "BRy"]
+    col1_reqd = col1_reqd.reset_index()
+    # T-Bil列のY座標を計算する
+    TBil_ycoord = df1.loc[(df1["Text"] == "T-Bi l")| (df1["Text"] == "T-Bil"),"BRy"]
+    TBil_ycoord = TBil_ycoord.reset_index()
+    TBil_ycoord = TBil_ycoord["BRy"][0]
+    # PLT列のY座標を計算する
+    PLT_ycoord = df1.loc[(df1["Text"] == "PLT"), "BRy"]
+    PLT_ycoord = PLT_ycoord.reset_index()
+    PLT_ycoord = PLT_ycoord["BRy"][0]
+    # CRE列のY座標を計算する
+    CRE_ycoord = df1.loc[(df1["Text"] == "CRE"), "BRy"]
+    CRE_ycoord = CRE_ycoord.reset_index()
+    CRE_ycoord = CRE_ycoord["BRy"][0]
+    # T-Bil列のY座標から＋－15ピクセル（行が斜めになっている可能性があるから）に離れているY座標をColumn2から探し出す。これはT-Bilに対する結果です。
+    TBil_res = df2.loc[(df2["BRy"]> TBil_ycoord-15)& (df2["BRy"]< TBil_ycoord+15)]
+    TBil_res = TBil_res.reset_index()
+    TBil_res = TBil_res["Text"][0]
+    # PLT列のY座標から＋－15ピクセルに離れているY座標をColumn2から探し出す。これはPLTに対する結果です（行が斜めになっている可能性があるから）
+    PLT_res = df2.loc[(df2["BRy"] > PLT_ycoord - 15) & (df2["BRy"] < PLT_ycoord + 15)]
+    PLT_res = PLT_res.reset_index()
+    PLT_res = PLT_res["Text"][0]
+
+    CRE_res = df2.loc[(df2["BRy"] > CRE_ycoord - 15) & (df2["BRy"] < CRE_ycoord + 15)]
+    CRE_res = CRE_res.reset_index()
+    CRE_res = CRE_res["Text"][0]
+
+    # PLT列のY座標から＋－30ピクセルに離れているY座標をColumn3から探し出す。これはPLTに対する単位です（行が斜めになっている可能性があるから）
+    units_PLT = df3.loc[(df3["BRy"] > PLT_ycoord - 30) & (df3["BRy"] < PLT_ycoord + 30)]
+    units_PLT = units_PLT.reset_index()
+    units_PLT_final = units_PLT.sum(axis = 0)["Text"]
+
+    # PLT列のY座標から＋－15ピクセルに離れているY座標をColumn4から探し出す。これはPLTに対する結果のL/Hです（行が斜めになっている可能性があるから）
+    PLT_res_LorH = df4.loc[(df4["BRy"] > PLT_ycoord - 15) & (df4["BRy"] < PLT_ycoord + 15)]
+    PLT_res_LorH = PLT_res_LorH.reset_index()
+
+    # T-Bil列のY座標から＋－15ピクセルに離れているY座標をColumn4から探し出すこれはT-Bilに対する結果のL / Hです
+    TBil_res_LorH = df4.loc[(df4["BRy"] > TBil_ycoord - 15) & (df4["BRy"] < TBil_ycoord + 15)]
+    TBil_res_LorH = TBil_res_LorH.reset_index()
+
+    CRE_res_LorH = df4.loc[(df4["BRy"] > CRE_ycoord - 15) & (df4["BRy"] < CRE_ycoord + 15)]
+    CRE_res_LorH = CRE_res_LorH.reset_index()
+    if len(PLT_res_LorH):
+        PLT_res_LorH = PLT_res_LorH["Text"][0]
+    else:
+        PLT_res_LorH = " "
+    if len(TBil_res_LorH):
+        TBil_res_LorH = TBil_res_LorH["Text"][0]
+    else:
+        TBil_res_LorH = " "
+    if len(CRE_res_LorH):
+        CRE_res_LorH = CRE_res_LorH["Text"][0]
+    else:
+        CRE_res_LorH = " "
+    return TBil_res, TBil_res_LorH, PLT_res, PLT_res_LorH, units_PLT_final, CRE_res, CRE_res_LorH
+
+
+def ocr_rapidpoint(im):
+    print("[INFO] OCR'ing RapidPoint image...")
+    # break the input languages into a comma separated list
+    langs = ["en"]
+    reader = Reader(langs, False)
+    results = reader.readtext(im, paragraph=False, detail=1)
+    df = pd.DataFrame(results, columns=["BBox", "Text", "Prob"])
+    df["BRy"] = df["BBox"].apply(lambda x :x[2][1])
+    df["TLx"] = df["BBox"].apply(lambda x :x[0][0])
+    df = df.sort_values(["TLx"])
+    df[["Text", "BRy", "TLx"]].to_csv("C:\\Users\\shimadzu\\Documents\\t-icu\\rp.csv",encoding='utf-8-sig')
+    reqd_text = df.loc[(df["Text"] == "pOz")| (df["Text"] == "poz")|(df["Text"] == "pO2"), "BRy"]
+    reqd_text = reqd_text.reset_index()
+    poz_ycoord = reqd_text["BRy"][0]
+    poz_res = df.loc[(df["BRy"] > poz_ycoord - 40) & (df["BRy"] < poz_ycoord + 40)]
+    poz_res = poz_res.reset_index()
+    poz_res = poz_res["Text"][1]
+    return poz_res
+
+
+
+
---
+++ initialize_ocr.py
@@ -0,0 +1,29 @@
+import cv2
+import easy_ocr
+
+im = cv2.imread("sample3.jpg", 1)
+
+#イメージのシェープからイメージの種類を決める。RapidPointの画像の幅は1500ピクセルを超えない。
+x, y = im.shape[:2]
+if (x<1500)|(y<1500):
+    #OCRをパケージ化した関数を呼ぶ、RapidPointのために定義した関数です。
+    a = easy_ocr.ocr_rapidpoint(im)
+    print("pO2 = ", a)
+
+else:
+    # OCRをパケージ化した関数を呼ぶ、採決結果フォームのOCRは違う関数を使用する
+    #FindRegions関数はまずフォームを読み込んで、読み取りたいColumnを識別する
+    c1, c2, c3, c4,c5, c6, c7, c8 = easy_ocr.divide_form_cols(im)
+    #Columnに分けてからOCRに進む。C1-C2は検査項目、C3-C4は結果、C5-C6は単位、C7-C8は結果のL/H
+    #上のような分け方が一番OCR精度が高い。
+    easy_ocr.ocr(im, c1, c2, c3, c4, c5, c6, c7, c8)
+    # 次は読み取りたいColumnから必要なテキスト（PLTとT-Bilの結果）を取り出す関数です。
+    a, b, c, d, e, f, g= easy_ocr.find_reqd_text()
+
+    #読み取った結果の必要な項目だけをピックアップして画面で表示する
+    print("T-Bil = ", a, b)
+    print("CRE = ", f, g)
+    print("PLT = ", c, d, e)
+
+cv2.waitKey(0)
+
